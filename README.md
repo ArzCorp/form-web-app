827a1b7b0c14a4db3fee2191ec66798801bcfb86
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2023-01-11 20:57:16 -0600

    🎉 initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Multi step form
+
+this is an aplication to simulate a multi step form, build with react js (next js) and tailwindcss
+
+# 🔖v1.0
---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const EMPTY_STRING = ''
+export const TRUE = true
+export const FALSE = false
+export const NULL = null
+export const _0 = 0
