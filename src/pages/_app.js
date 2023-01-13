f92b5ff6c4c19a9cc771684ed51a1be857c3787a
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2023-01-12 21:29:46 -0600

    🔧 add absolute imports

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import 'tailwindcss/tailwind.css'
-import '../styles/index.css'
+import 'styles/index.css'
 
 export default function App({ Component, pageProps }) {
 	return <Component {...pageProps} />
