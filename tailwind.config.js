23ed8fa798e1a5ade475453b74cd601286b9a708
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2023-01-12 20:52:44 -0600

    💄 add colors and font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,31 @@ module.exports = {
 		'./src/components/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				light: '#EFF5FF',
+				border: '#D6D9E6',
+				denim: '#022959',
+				grey: '#9699AA',
+				'light-blue': '#ABBCFF',
+				'light-grey': '#D6D9E6',
+				orange: '#FFAF7E',
+				pink: '#F9818E',
+				purple: '#483EFF',
+				'red-error': '#EE374A',
+				'sky-blue': '#BEE2FD',
+				'very-light-grey': '#F8F9FF',
+				white: '#FFFFFF',
+			},
+			fontSize: {
+				'heading-l': ['2rem', 'auto'],
+				'heading-m': ['1.5rem', '1.724rem'],
+				'body-xl': ['1rem', '1.563rem'],
+				'body-l': ['0.938rem', '1.078rem'],
+				'body-m': ['0.875rem', 'auto'],
+				'body-s': ['0.75rem', 'auto'],
+			},
+		},
 	},
 	plugins: [],
 }
