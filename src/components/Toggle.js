661ebb4213368db59f674e3f368cec4efcaecead
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2023-01-15 18:33:36 -0600

    💄 add toggle component

---
+++ src/components/Toggle.js
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import { FALSE } from 'utils/constants'
+
+export default function Toggle({ onClick = () => {}, name, ...props }) {
+	const [active, setActive] = useState(FALSE)
+	const activeStyles = active ? 'translate-x-[18px]' : ''
+
+	const handleToggle = () => {
+		setActive(!active)
+		onClick({
+			target: {
+				value: !active,
+				name,
+			},
+		})
+	}
+	return (
+		<div {...props} className="w-[38px] bg-denim p-1 rounded-[10px]">
+			<div
+				className={`${activeStyles} w-[12px] h-[12px] bg-white rounded-[50%] transition-all`}
+				onClick={handleToggle}
+			></div>
+		</div>
+	)
+}
