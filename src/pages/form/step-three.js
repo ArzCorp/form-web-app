d8b37e5f8206bddc33962ad2526fa0e25e85b7a3
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2023-01-16 22:46:26 -0600

    💄 add step three page

--- src/pages/form/step-three.js
+++ src/pages/form/step-three.js
@@ -1,9 +1,20 @@
+import Checkbox from 'components/Checkbox'
 import Layout from 'components/Layout'
+import StepLayout from 'components/StepLayout'
+import { ADDONS } from 'utils/constants'
 
 export default function stepThree() {
 	return (
 		<Layout title="Form | Step - Three">
-			<h3>Step - Three</h3>
+			<StepLayout
+				title="Pick add-ons"
+				description="Add-ons help enhance your gaming experience."
+			/>
+			<div className="flex flex-col gap-3">
+				{ADDONS.map((additional) => (
+					<Checkbox key={additional.key} {...additional} />
+				))}
+			</div>
 		</Layout>
 	)
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -46,3 +46,24 @@ export const PLANS = [
 		icon: '/icons/icon-pro.svg',
 	},
 ]
+
+export const ADDONS = [
+	{
+		id: 1,
+		title: 'Online service',
+		description: 'Access to multiplayer games',
+		price: 1,
+	},
+	{
+		id: 2,
+		title: 'Larger storage',
+		description: 'Extra 1TB of cloud save',
+		price: 2,
+	},
+	{
+		id: 3,
+		title: 'Customizable profile',
+		description: 'Custom theme on your profile',
+		price: 2,
+	},
+]
