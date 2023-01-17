7e1b357aedb9dcda375881f51d9fe6f6676c68bb
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2023-01-16 22:58:26 -0600

    ✨ add initial loader

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,18 @@
-import { Fragment } from 'react'
+import { Fragment, useCallback, useEffect } from 'react'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 export default function Home() {
+	const { push } = useRouter()
+
+	const moveToHome = useCallback(() => {
+		push('/form/step-one')
+	}, [push])
+
+	useEffect(() => {
+		moveToHome()
+	}, [moveToHome])
+
 	return (
 		<Fragment>
 			<Head>
@@ -10,9 +21,13 @@ export default function Home() {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.png" />
 			</Head>
-			<main className="bg-black text-white min-h-screen flex justify-center items-center">
-				<p className="text-3xl">Multi step form - 🔖v1.0</p>
-			</main>
+			<div className="flex items-center justify-center min-h-screen">
+				<div className="text-purple font-bold text-xl p-4 bg-very-light-grey rounded-lg max-w-min animate-bounce duration-75">
+					<p>MULTI</p>
+					<p>STEP</p>
+					<p>FORM</p>
+				</div>
+			</div>
 		</Fragment>
 	)
 }
